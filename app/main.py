12e6cc74f388db089c34aac1e825ff06aaf568cf
Jon7204 <owensjonathan@outlook.com>
2026-03-02 14:18:43 +0000

    Implemented avaerage price function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Query
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 
@@ -8,6 +8,7 @@ from .models import Property
 app = FastAPI()
 
 
+# Proper dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -18,10 +19,30 @@ def get_db():
 
 @app.get("/")
 def root():
-    return {"message": "Housing API is running"}
+    return {"message": "Housing API running"}
 
 
 @app.get("/properties/count")
-def property_count(db: Session = next(get_db())):
+def property_count(db: Session = Depends(get_db)):
     count = db.query(func.count(Property.id)).scalar()
     return {"total_properties": count}
+
+
+@app.get("/properties/average-price")
+def average_price(
+    postcode: str = Query(...),
+    db: Session = Depends(get_db)
+):
+    avg_price = (
+        db.query(func.avg(Property.price))
+        .filter(Property.postcode == postcode)
+        .scalar()
+    )
+
+    if avg_price is None:
+        return {"postcode": postcode, "average_price": None}
+
+    return {
+        "postcode": postcode,
+        "average_price": round(avg_price, 2)
+    }
