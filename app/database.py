4657012f1dca41552d834a0ddf41eb0159da0267
Jon7204 <owensjonathan@outlook.com>
2026-03-13 16:49:05 +0000

    Updated database_URLs

--- app/database.py
+++ app/database.py
@@ -3,9 +3,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 
-# If using local PostgreSQL, the URL would be:
+# If following 4. Option 1 use:
 #DATABASE_URL = "postgresql://localhost/housing_db"
-# If using external PostgreSQL, the URL would be:
+
+# If following 4. Option 2 use:
+#DATABASE_URL = "postgresql://housing_db_k5j7_user:nUNlRanASFI1aQFnAR6PKEWFoAv1t6HM@dpg-d6q2d0aa214c73f8vsdg-a.frankfurt-postgres.render.com/housing_db_k5j7"
+
+# URL for External server to host:
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
